f41cbc84185268cfdf66d200ad0f476f2e3fe0fb
Papa Babacar BA <babacarba.moon@gmail.com>
2020-10-26 20:59:13 +0000

    fix build + tsconfig

--- remotedev-utils.d.ts
+++ remotedev-utils.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export declare module 'remotedev-utils' {
   export function evalMethod(payload: any, state: any)
 }
--- remotedev.d.ts
+++ remotedev.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export declare module 'remotedev' {
   export function connectViaExtension(options: any)
   export function extractState(message: any)
